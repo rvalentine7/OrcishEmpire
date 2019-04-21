7ce9d88325990bd3083077d402616c45fdaa7c45
rvalentine7 <rgv9251@gmail.com>
2019-04-20 23:19:56 -0400

    Updated UI for Sickness and Maintenance
    Sickness and Maintenance have been added to relevant buildings.  Sickness will show house inhabitants or employment workers who are sick.  Maintenance will be for repairs when repairing buildings is added.

---
+++ Assets/Scripts/UI/ChangeTabs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTabs : MonoBehaviour
+{
+    public GameObject panelToActivate;
+    public GameObject panelToDeactivate;
+    public GameObject newLargeButton;
+    public GameObject newSmallButton;
+    public GameObject oldLargeButton;
+    public GameObject oldSmallButton;
+
+    /**
+     * Change the buttons and panels being displayed on the popup
+     */
+    public void changePanels()
+    {
+        panelToActivate.SetActive(true);
+        panelToDeactivate.SetActive(false);
+        newLargeButton.SetActive(true);
+        newSmallButton.SetActive(true);
+        oldLargeButton.SetActive(false);
+        oldSmallButton.SetActive(false);
+    }
+}
